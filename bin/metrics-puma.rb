ff2284ba37b029552a79eb64c13052096ba6ba13
Steve Valaitis <steve@digitalnothing.com>
2017-08-20 15:10:14 -0500

    parse v3.x state file format

--- bin/metrics-puma.rb
+++ bin/metrics-puma.rb
@@ -29,7 +29,6 @@
 require 'sensu-plugin/metric/cli'
 
 require 'json'
-require 'puma/configuration'
 require 'socket'
 require 'yaml'
 
@@ -46,16 +45,41 @@ class PumaMetrics < Sensu::Plugin::Metric::CLI::Graphite
          long: '--state-file SOCKET',
          default: '/tmp/puma.state'
 
+  option :control_auth_token,
+         description: 'The auth token to connect to the control server with',
+         long: '--auth-token TOKEN'
+
+  option :control_url,
+         description: 'The control_url the puma control server is listening on',
+         long: '--control-url PATH'
+
+  def control_auth_token
+    @control_auth_token ||= (config[:control_auth_token] || puma_options[:control_auth_token])
+  end
+
+  def control_url
+    @control_url ||= (config[:control_url] || puma_options[:control_url])
+  end
+
   def puma_options
     @puma_options ||= begin
       return nil unless File.exist?(config[:state_file])
-      YAML.load_file(config[:state_file])['config'].options
+      state = load_puma_state(config[:state_file])
+
+      if state.has_key?('config')
+        # state is < v3.0.0
+        opts = state['config']['options']
+        {control_url: opts[:control_url], control_auth_token: opts[:control_auth_token]}
+      else
+        # state is >= v3.0.0
+        {control_url: state['control_url'], control_auth_token: state['control_auth_token']}
+      end
     end
   end
 
   def puma_stats
-    stats = Socket.unix(puma_options[:control_url].gsub('unix://', '')) do |socket|
-      socket.print("GET /stats?token=#{puma_options[:control_auth_token]} HTTP/1.0\r\n\r\n")
+    stats = Socket.unix(control_url.gsub('unix://', '')) do |socket|
+      socket.print("GET /stats?token=#{control_auth_token} HTTP/1.0\r\n\r\n")
       socket.read
     end
 
@@ -67,5 +91,13 @@ class PumaMetrics < Sensu::Plugin::Metric::CLI::Graphite
       output "#{config[:scheme]}.#{k}", v
     end
     ok
+  end
+
+  private
+
+  def load_puma_state(path)
+    raw = File.read(path)
+    sanitized = raw.gsub(/!ruby\/object:.*$/, '')
+    YAML.load(sanitized)
   end
 end
